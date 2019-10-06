4436aa15b02a00265a485189627d044960c9c362
John Osborn <josborn5@gmail.com>
2019-10-05 23:43:21 -0500

    updating build.bat for VS2017 and the new location of the vsvarsall.bat file.

--- build.bat
+++ build.bat
@@ -1,6 +1,11 @@
 REM https://youtu.be/IztkFkg2Y-4?t=1844
 
-call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+REM VS 2015:
+REM call "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+
+REM VS 2017:
+call "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
+
 
 SET CODE_DIR=code
 SET OUTPUT_DIR=bin
